cf9a414a46eeabd73b7b046e8f238aaeb331d773
Jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2024-11-08 16:27:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function abc() {
+    var myValue = document.getElementById("xyz");
+    if (myValue.value == "hi") {
+        alert("Hello Dear");
+    } else {
+        alert("You Enter Wrong Words, Please Enter " + myValue.value);
+    }
+}
